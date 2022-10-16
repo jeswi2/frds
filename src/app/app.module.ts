7b3389f3a1bb78e993710ae815ca8ce96b032fc2
jeswi2 <jeswimathewm@gmail.com>
2022-10-16 22:22:21 +0530

    ade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddfriendsComponent } from './addfriends/addfriends.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const appRoutes:Routes=[
+  {
+    path:"",component:AddfriendsComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddfriendsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]

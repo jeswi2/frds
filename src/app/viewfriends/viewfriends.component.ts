9bd54555205355415be2a1f37b8e4d9236b1c3df
jeswi2 <jeswimathewm@gmail.com>
2022-10-16 23:16:30 +0530

    ser

--- src/app/viewfriends/viewfriends.component.ts
+++ src/app/viewfriends/viewfriends.component.ts
@@ -10,12 +10,16 @@ export class ViewfriendsComponent implements OnInit {
 
   constructor(private myapi:ApiService) {
     this.fetchData()
+    
    }
+   status:boolean=false
+
 
   fetchData=()=>{
     this.myapi.viewFriend().subscribe(
       (data)=>{
         this.friendsData=data
+        this.status=true
       }
     )
   }
